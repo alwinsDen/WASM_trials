e30655190a83cf5ce718bd9690d4c819e65ee431
alwinsDen <jsmadesimple@gmail.com>
2024-03-18 13:30:44 +0530

    ui chngs

---
+++ js_manual_source_files/video_process.js
@@ -0,0 +1,5 @@
+const pdfProcessDoc = document.querySelector(".videoInputClass");
+pdfProcessDoc.addEventListener("change",dataProcess);
+function dataProcess(){
+    console.log("Data profiling.")
+}

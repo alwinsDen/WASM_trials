540c580c49d9bfa956ed0557cff85ff30071060a
alwinvarghese <jsmadesimple@gmail.com>
2024-03-22 10:34:06 +0530

    wasm

--- js_manual_source_files/video_process.js
+++ js_manual_source_files/video_process.js
@@ -1,5 +1,7 @@
-const pdfProcessDoc = document.querySelector(".videoInputClass");
-pdfProcessDoc.addEventListener("change",dataProcess);
-function dataProcess(){
-    console.log("Data profiling.")
-}
+const videoDomEle = document.querySelector(".videoInputClass");
+videoDomEle.addEventListener("change", function (){
+    let mediaFile = URL.createObjectURL(videoDomEle.files[0]);
+    //select the video display element
+    let video = document.querySelector(".videoPainter");
+    video.src = mediaFile;
+})
